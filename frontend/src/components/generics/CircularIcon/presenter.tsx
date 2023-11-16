7d11bc421a740d77217a0e5217ef53b5ba0539e5
Yamashita <jey.togo@gmail.com>
2023-11-16 17:59:55 +0900

    fix: 画像表示エラーが起きる箇所を修正

--- frontend/src/components/generics/CircularIcon/presenter.tsx
+++ frontend/src/components/generics/CircularIcon/presenter.tsx
@@ -3,15 +3,18 @@ import {Image} from 'react-native';
 import styles from './style';
 
 type Props = {
-  url: any;
+  url: string;
   styles: any;
 };
 
 const CircularIconPresenter = (props: Props) => {
+
   return (
     <Image
       source={{
-        uri: 'https://firebasestorage.googleapis.com/v0/b/avocado-test-5e236.appspot.com/o/character_icons%2Fb.png?alt=media&token=038841ca-6718-4acc-bc2b-5bb943cb9297',
+        uri:
+          typeof(props.url) === 'string' ? props.url :
+          'https://firebasestorage.googleapis.com/v0/b/avocado-test-5e236.appspot.com/o/character_icons%2Fb.png?alt=media&token=038841ca-6718-4acc-bc2b-5bb943cb9297' ,
       }}
       style={[styles.circularImage, props.styles]}
     />
