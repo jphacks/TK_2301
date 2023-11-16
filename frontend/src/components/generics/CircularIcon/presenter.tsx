21fe8974c2083afed7637ed1977cfe1c3f5c4843
Yamashita <jey.togo@gmail.com>
2023-11-16 18:25:29 +0900

    feat: アイコン画像表示にエラーハンドリング追加

--- frontend/src/components/generics/CircularIcon/presenter.tsx
+++ frontend/src/components/generics/CircularIcon/presenter.tsx
@@ -13,7 +13,7 @@ const CircularIconPresenter = (props: Props) => {
     <Image
       source={{
         uri:
-          typeof(props.url) === 'string' ? props.url :
+          (props.url !== null && props.url !== undefined && typeof(props.url) === 'string' && props.url !== '' ) ? props.url :
           'https://firebasestorage.googleapis.com/v0/b/avocado-test-5e236.appspot.com/o/character_icons%2Fb.png?alt=media&token=038841ca-6718-4acc-bc2b-5bb943cb9297' ,
       }}
       style={[styles.circularImage, props.styles]}
