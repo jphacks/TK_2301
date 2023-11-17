e2bbbc3d94c0d82920167359d773cefc57e375d5
Numano-cdlab <ma23145@shibaura-it.ac.jp>
2023-11-17 23:51:59 +0900

    delete: lambda function

--- ai_generator/scenario_generator/calcToken.py
+++
@@ -1,9 +0,0 @@
-# 請求金額を計算する（ドル）
-def calcBilling(response, model = "gpt-3.5-turbo"):
-	if model == "gpt-4":
-		return "{:.10g}".format(0.03 * 0.001 * response["usage"]["prompt_tokens"] + 0.06 * 0.001 * response["usage"]["completion_tokens"])
-	elif model == "gpt-3.5-turbo":
-		return "{:.10g}".format(0.0015 * 0.001 * response["usage"]["prompt_tokens"] + 0.002 * 0.001 * response["usage"]["completion_tokens"])
-	else:
-		print("Error: invalid model name")
-		return -1
