5bddbdfa31f59ea7ed38afca55ade926371148da
Yamashita <jey.togo@gmail.com>
2023-11-18 14:06:52 +0900

    fix: 画像生成画面の調整

--- avocado-front/src/pages/CreateScenario/FetchingModal/presenter.tsx
+++ avocado-front/src/pages/CreateScenario/FetchingModal/presenter.tsx
@@ -4,18 +4,18 @@ import styles from './style';
 import Spinner from 'react-native-loading-spinner-overlay';
 import {useCreateScenario} from '../createScenario';
 
-type Props = {
-} & ContainerProps;
+type Props = {} & ContainerProps;
 
 const FetchingModalPresenter = ({textContent}: Props) => {
   const {isFetching} = useCreateScenario();
 
   return (
-      <Spinner
-        visible={isFetching}
-        textContent={textContent}
-        textStyle={styles.text}
-      />
+    <Spinner
+      visible={isFetching}
+      textContent={textContent}
+      textStyle={styles.text}
+      overlayColor="rgba(0, 0, 0, 0.6)"
+    />
   );
 };
 
--- avocado-front/src/pages/CreateScenario/ImageCreate/presenter.tsx
+++ avocado-front/src/pages/CreateScenario/ImageCreate/presenter.tsx
@@ -41,117 +41,119 @@ const ImageCreatePresenter = ({
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
   return (
-    <ScrollView style={styles.container}>
-      <FetchingModal textContent={'生成中...'} />
+    <ScrollView>
+      <View style={styles.container}>
+        <FetchingModal textContent={'生成中...'} />
 
-      {fetchedImage &&
-        (targetImageType === ImageType.FloorMap ? (
-          <View style={styles.proposedImageForFloorMapContainer}>
-            <View style={styles.proposedImageContainer}>
-              <PagerView
-                style={styles.viewPager}
-                initialPage={0}
-                onPageSelected={event => {
-                  setFocusedImageUri('');
-                  const {position} = event.nativeEvent;
-                  setSelectedIndex(position);
-                }}>
-                {candidateImageUri.map(uri => {
+        {fetchedImage &&
+          (targetImageType === ImageType.FloorMap ? (
+            <View style={styles.proposedImageForFloorMapContainer}>
+              <View style={styles.proposedImageContainer}>
+                <PagerView
+                  style={styles.viewPager}
+                  initialPage={0}
+                  onPageSelected={event => {
+                    setFocusedImageUri('');
+                    const {position} = event.nativeEvent;
+                    setSelectedIndex(position);
+                  }}>
+                  {candidateImageUri.map(uri => {
+                    return (
+                      <TouchableOpacity
+                        key={`floorMap.${uri}`}
+                        onPress={() => {
+                          setFocusedImageUri(uri);
+                        }}>
+                        <Image
+                          style={[
+                            styles.proposedImageForFloorMap,
+                            {borderWidth: focusedImageUri === uri ? 2 : 0},
+                          ]}
+                          source={{uri}}
+                        />
+                      </TouchableOpacity>
+                    );
+                  })}
+                </PagerView>
+              </View>
+
+              <View style={styles.proposedImageDotsContainer}>
+                {candidateImageUri.map((uri, index) => {
                   return (
-                    <TouchableOpacity
-                      key={uri}
-                      onPress={() => {
-                        setFocusedImageUri(uri);
-                      }}>
-                      <Image
-                        key={uri}
-                        style={[
-                          styles.proposedImageForFloorMap,
-                          {borderWidth: focusedImageUri === uri ? 2 : 0},
-                        ]}
-                        source={{uri}}
-                      />
-                    </TouchableOpacity>
+                    <View
+                      key={`proposedImageDots.${index}`}
+                      style={[
+                        styles.dot,
+                        {
+                          marginRight:
+                            index === candidateImageUri.length - 1 ? 0 : 8,
+                          backgroundColor:
+                            selectedIndex === index ? 'white' : '#6e6d6d',
+                        },
+                      ]}></View>
                   );
                 })}
-              </PagerView>
+              </View>
             </View>
-
-            <View style={styles.proposedImageDotsContainer}>
-              {candidateImageUri.map((uri, index) => {
+          ) : (
+            <View style={styles.proposedImageContainer}>
+              {candidateImageUri.map(uri => {
                 return (
-                  <View
-                    style={[
-                      styles.dot,
-                      {
-                        marginRight:
-                          index === candidateImageUri.length - 1 ? 0 : 8,
-                        backgroundColor:
-                          selectedIndex === index ? 'white' : '#6e6d6d',
-                      },
-                    ]}></View>
+                  <TouchableOpacity
+                    key={uri}
+                    onPress={() => {
+                      setFocusedImageUri(uri);
+                    }}>
+                    <Image
+                      key={uri}
+                      style={[
+                        styles.proposedImage,
+                        {borderWidth: focusedImageUri === uri ? 2 : 0},
+                      ]}
+                      source={{uri}}
+                    />
+                  </TouchableOpacity>
                 );
               })}
             </View>
-          </View>
-        ) : (
-          <View style={styles.proposedImageContainer}>
-            {candidateImageUri.map(uri => {
-              return (
-                <TouchableOpacity
-                  key={uri}
-                  onPress={() => {
-                    setFocusedImageUri(uri);
-                  }}>
-                  <Image
-                    key={uri}
-                    style={[
-                      styles.proposedImage,
-                      {borderWidth: focusedImageUri === uri ? 2 : 0},
-                    ]}
-                    source={{uri}}
-                  />
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-        ))}
+          ))}
 
-      {focusedImageUri !== '' && (
-        <View style={styles.button}>
-          <PrimaryButton
-            width={320}
-            onPress={onPressDecideImage}
-            text={'この画像を使う'}
-          />
+        {focusedImageUri !== '' && (
+          <View style={styles.button}>
+            <PrimaryButton
+              width={320}
+              onPress={onPressDecideImage}
+              text={'この画像を使う'}
+            />
+          </View>
+        )}
+
+        <View>
+          <Text>{image}</Text>
+          <Text style={styles.text}>作りたい画像のキーワード入れましょう</Text>
         </View>
-      )}
 
-      <View>
-        <Text>{image}</Text>
-        <Text style={styles.text}>作りたい画像のキーワード入れましょう</Text>
-      </View>
+        <TextInput
+          style={[styles.input]}
+          placeholder="中世風のナイフ"
+          placeholderTextColor="#888888"
+          onChangeText={text => onChangeText(text)}
+        />
 
-      <TextInput
-        style={[styles.input]}
-        placeholder="中世風のナイフ"
-        placeholderTextColor="#888888"
-        onChangeText={text => onChangeText(text)}
-      />
+        <View>
+          <Text style={[styles.text, {textAlign: 'left'}]}>キーワードの例</Text>
+          <Text style={styles.sm}>
+            こんな感じで入力すると、よりイメージに近い画像を作成 できます！
+          </Text>
 
-      <View>
-        <Text style={[styles.text, {textAlign: 'left'}]}>キーワードの例</Text>
-        <Text style={styles.sm}>
-          こんな感じで入力すると、よりイメージに近い画像を作成 できます！
-        </Text>
+          <Text style={styles.bullet}>・中世風のナイフ</Text>
+          <Text style={styles.bullet}>・可愛いスマホ</Text>
+          <Text style={styles.bullet}>・SFな銃</Text>
+        </View>
 
-        <Text style={styles.bullet}>・中世風のナイフ</Text>
-        <Text style={styles.bullet}>・可愛いスマホ</Text>
-        <Text style={styles.bullet}>・SFな銃</Text>
-      </View>
-
-      <View style={styles.button}>
-        <PrimaryButton width={320} onPress={onPress} text={'画像を生成'} />
+        <View style={styles.button}>
+          <PrimaryButton width={320} onPress={onPress} text={'画像を生成'} />
+        </View>
       </View>
     </ScrollView>
   );
